20b85ba18234e05a73242765d4b348ccc6567b69
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-02 01:50:50 +0530

    Learn React from begining

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>HELLO, WORLD!</h1>
+      <h2>Welcome to my React App</h2>
+    </div>
+  )
+}
+
+export default App

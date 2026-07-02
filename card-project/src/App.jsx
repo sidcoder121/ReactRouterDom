ce4cbb19a40ee2e2d061ebfa5b71d8448f14695e
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-03 04:17:25 +0530

    complete all CSS of project

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -5,17 +5,19 @@ export const App = () => {
   return (
     <div className="parent">
       <div className="card">
+        <div>
         <div className="top">
           <img src="https://tse1.mm.bing.net/th/id/OIP.douAQqLQCydHXDqsPfOcpwHaEK?pid=Api&h=220&P=0" alt="amazon logo " />
-          <button>Save <Bookmark/></button>
+          <button>Save <Bookmark size={12}/></button>
         </div>
         <div className="center">
           <h3>Amazon <span>5 days ago</span></h3>
           <h2>Senior UI/UX designer</h2>
-          <div>
+          <div className="tag">
             <h4>Part Time</h4>
             <h4>Senior Level</h4>
           </div>
+        </div>
         </div>
         <div className="bottom">
             <div>

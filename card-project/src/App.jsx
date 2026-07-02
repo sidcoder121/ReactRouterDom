88c9b250a25014f0ea3f855637f9f2151361f69a
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-03 04:27:12 +0530

    create a component fie name Card.jsx

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+import Card from './components/Card.jsx'
 
 export const App = () => {
   return (
     <div className="parent">
-      <div className="card">
-        <div>
-        <div className="top">
-          <img src="https://tse1.mm.bing.net/th/id/OIP.douAQqLQCydHXDqsPfOcpwHaEK?pid=Api&h=220&P=0" alt="amazon logo " />
-          <button>Save <Bookmark size={12}/></button>
-        </div>
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX designer</h2>
-          <div className="tag">
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-        </div>
-        <div className="bottom">
-            <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
-            </div>
-              <button>Apply Now</button>
-          </div>
-      </div>
+      <Card/>
     </div>
   )
 }

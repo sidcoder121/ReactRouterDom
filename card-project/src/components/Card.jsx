cca58e0da99e7c418595b91f1b33a588d08b138f
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-03 05:18:55 +0530

    Card-Project-Done

--- card-project/src/components/Card.jsx
+++ card-project/src/components/Card.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
    <div className="card">
         <div>
         <div className="top">
-          <img src={}alt="amazon logo " />
+          <img src={props.brandLogo}alt="amazon logo " />
           <button>Save <Bookmark size={12}/></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX designer</h2>
+          <h3>{props.companyName} <span>{props.datePosted}</span></h3>
+          <h2>{props.post}</h2>
           <div className="tag">
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
         </div>
         <div className="bottom">
             <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
+              <h3>${props.pay}/hr</h3>
+              <p>{props.location}</p>
             </div>
               <button>Apply Now</button>
           </div>

63425059e68d056c39955e6cf3fa867ac00b7b30
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-03 03:23:37 +0530

    write the HTML for card project

---
+++ card-project/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+export const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.douAQqLQCydHXDqsPfOcpwHaEK?pid=Api&h=220&P=0" alt="amazon logo " />
+          <button>Save <Bookmark/></button>
+        </div>
+        <div className="center">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/UX designer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai, India</p>
+            </div>
+              <button>Apply Now</button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App

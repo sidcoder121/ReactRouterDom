3b62cbb5b0f25e67d6fc6337f1a9520e46cfd688
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-04 22:28:30 +0530

    Start learning Functions in React

---
+++ FunctionsINreact/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello,Siddharth</h1>
+      <button>Change User</button>
+    </div>
+  )
+}
+
+export default App

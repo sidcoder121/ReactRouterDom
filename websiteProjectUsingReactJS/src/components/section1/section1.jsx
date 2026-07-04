6e48ede42e657645fb054295205b99948bf7024b
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-04 14:34:10 +0530

    Start UI project and create 2 section

---
+++ websiteProjectUsingReactJS/src/components/section1/section1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Section1 = () => {
+  return (
+    <div className="h-screen w-full bg-blue-950">section1</div>
+  )
+}
---
+++ websiteProjectUsingReactJS/src/components/section2/section2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Section2 = () => {
+  return (
+    <div className="h-screen w-full bg-emerald-500">
+    Section2
+    </div>
+  )
+}

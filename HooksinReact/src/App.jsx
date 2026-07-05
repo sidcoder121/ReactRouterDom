d60816732295cf0ae0b9044b601cc9d230d38bec
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-05 22:55:27 +0530

    compete Count App using usestate

--- HooksinReact/src/App.jsx
+++ HooksinReact/src/App.jsx
@@ -13,13 +13,17 @@ export const App = () => {
   }
 
   return (
+    <>
     <div>
     <center>
      <h1>{num}</h1>
      <button onClick={increase}>Increase</button>
      <button onClick={decrease}>Decrease</button>
      </center>
+     
     </div>
+   
+    </>
   )
 }
 

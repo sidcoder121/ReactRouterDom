250f7a4789c2c5040be15378a9e7712ef9b679d3
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-07 13:57:34 +0530

    start learning form handling

---
+++ FormHandlingReact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const  App= () => {
+  return (
+    <div>
+      <form>
+        <input type='text'></input>
+        <input type='text'></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}

65ef32af162c45b69ea03c327b705ad0af3fde03
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-08 13:26:35 +0530

    create div foe show notes

--- NoteAppProject/src/App.jsx
+++ NoteAppProject/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 export const App = () => {
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log('Form Submited')
+  }
   return (
-    <div className='h-screen bg-black text-white '>
-      <form className='flex items-start justify-between  p-10 '>
-        <div className='w-1/2  gap-4 flex items-start flex-col'>
+    <div className='h-screen bg-black text-white  p-10 '>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+
+      }} className='flex items-start justify-between flex-col gap-4'>
           <input type='text' 
         placeholder='Enter Notes Heading'
-        className='p-5 w-full border-2 rounded-2xl outline-none '></input>
+        className='p-5 w-full font-medium border-2 rounded-2xl outline-none '></input>
 
         <textarea
         placeholder='Enter Details'
@@ -15,10 +22,12 @@ export const App = () => {
         name=''
         id=''>
         </textarea>
-        <button className='bg-white text-black h-15 w-full'>Add Note</button>
-        </div>
+        <button className='bg-white text-black h-15 font-medium w-full'>Add Note</button>
         
       </form>
+      <div className='flex flex-wrap p-10'>
+        <div className='h-32 w-32 rounded-2xl bg-amber-50'></div>
+      </div>
 
     </div>
   )

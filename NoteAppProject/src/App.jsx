2f4ed57c676f5b1f9cb4e2a6e51d61ef7be528df
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-08 12:45:02 +0530

    start two way binding

--- NoteAppProject/src/App.jsx
+++ NoteAppProject/src/App.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 export const App = () => {
   return (
     <div className='h-screen bg-black text-white '>
-      <form className='flex items-start flex-col gap-4 p-10 '>
-        <input type='text' 
+      <form className='flex items-start justify-between  p-10 '>
+        <div className='w-1/2  gap-4 flex items-start flex-col'>
+          <input type='text' 
         placeholder='Enter Notes Heading'
-        className='p-5 w-1/2 border-2 rounded-2xl '></input>
+        className='p-5 w-full border-2 rounded-2xl outline-none '></input>
 
-        <input type='text'
+        <textarea
         placeholder='Enter Details'
-        className='p-5 w-1/2 border-2 rounded-2xl h-25'
+        className='p-5 w-full border-2 rounded-2xl h-40 outline-none'
         name=''
         id=''>
-        </input>
-        <button className='bg-white text-black h-15 w-1/2'>Add Note</button>
+        </textarea>
+        <button className='bg-white text-black h-15 w-full'>Add Note</button>
+        </div>
+        
       </form>
 
     </div>

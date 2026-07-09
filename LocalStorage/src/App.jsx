4a94cec65b39209d5a6b7729a452c567e5e2983f
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-09 16:57:26 +0530

    learn localstorage and use JSON.stringify and setItem and getItem.

---
+++ LocalStorage/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const App = () => {
+
+  const user={
+    username:'Siddharth',
+    age:'24',
+    city:'Guna',
+  }
+  localStorage.setItem('user',JSON.stringify(user))
+
+  let get = localStorage.getItem('user')
+  console.log(get)
+  return (
+    <div>App</div>
+  )
+}
+
+export default App

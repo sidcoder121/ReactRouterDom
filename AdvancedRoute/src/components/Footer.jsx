e2dbfc569d67971452b6c521ddd70e72c45bed98
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-11 15:01:26 +0530

    create Routes for my website

---
+++ AdvancedRoute/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='flex w-full items-center justify-center bg-cyan-600 absolute bottom-0 '>This is Footer</div>
+  )
+}
+export default Footer
